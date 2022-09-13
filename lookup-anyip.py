5daa33e937e7113e681ec58d3b6645c9c8c829de
Richard Greville <richard.greville@opticoreit.com>
2022-09-13 19:09:13 +0100

    init

--- lookup-anyip.py
+++ lookup-anyip.py
@@ -1,23 +1,15 @@
 import requests
+import os
 from requests.structures import CaseInsensitiveDict
 
+token = os.environ['IPINFO_TOKEN']
+
 ip = "8.8.8.8"
-token="34c2af50f47083"
-
-#url = "https://ipinfo.io/8.8.8.8"
-
+#token ="34c2af50f47083"
 ipinfo = "https://ipinfo.io/"
-
 url = ipinfo+ip
-
-print(url)
-
-
-headers = CaseInsensitiveDict()
-headers["User-Agent"] = "curl/7.68.0"
-headers["Authorization"] = "Bearer {34c2af50f47083}"
-
-
+headers = {}
+headers["Authorization"] = f"Bearer {token}"
 resp = requests.get(url, headers=headers)
 
-print(resp.status_code)
+print(resp.json())
--- lookup-asn.py
+++ lookup-asn.py
@@ -13,8 +13,3 @@ country = data['country']
 region = data['region']
 
 print('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
-
-
-
-
-
