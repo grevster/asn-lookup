25f6b77ac9f70d2919de41b28923ac88689546ef
Richard Greville <richard.greville@opticoreit.com>
2022-09-14 07:12:29 +0100

    init

--- lookup-anyip.py
+++ lookup-anyip.py
@@ -1,11 +1,9 @@
 import requests
 import os
-from requests.structures import CaseInsensitiveDict
 
 token = os.environ['IPINFO_TOKEN']
 
 ip = "8.8.8.8"
-#token ="34c2af50f47083"
 ipinfo = "https://ipinfo.io/"
 url = ipinfo+ip
 headers = {}
